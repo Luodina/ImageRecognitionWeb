a1db133bf71306e581569440016531e6a2b00b9a
2847624779@qq.com <leng0607ning>
2017-05-12 17:19:57 +0800

    页面调试

--- Basic/app/scripts/controllers/dataExplore/data.js
+++ Basic/app/scripts/controllers/dataExplore/data.js
@@ -8,6 +8,12 @@ angular.module('basic')
     $scope.processing = $filter('translate')('web_common_data_explore_003');
     $scope.source = $filter('translate')('web_common_data_explore_001');
     $scope.report = $filter('translate')('web_common_data_explore_002');
+    $scope.datamation = $filter('translate')('web_common_data_explore_004');
+    $scope.defaultData = $filter('translate')('web_common_data_explore_005');
+    $scope.datafileimport = $filter('translate')('web_common_data_explore_006');
+    $scope.fileimport = $filter('translate')('web_common_data_explore_007');
+    $scope.datapreview = $filter('translate')('web_common_data_explore_008');
+
     $scope.names = {name1:"111"};
     $scope.tab = "source";
     $scope.goReport = function(item){
--- Basic/app/scripts/message_en.js
+++ Basic/app/scripts/message_en.js
@@ -17,5 +17,14 @@ angular.module('basic').config(['$translateProvider', function($translateProvide
     'web_common_data_explore_001': 'Data Source',
     'web_common_data_explore_002': 'Data Report',
     'web_common_data_explore_003': 'Data Processing',
+    'web_common_data_explore_004': 'Data datamation demo',
+    'web_common_data_explore_005': 'Default data source Data source (CDM interface) configured with parameter settings',
+    'web_common_data_explore_006': 'Data file import',
+    'web_common_data_explore_007': 'import',
+    'web_common_data_explore_008': 'Data preview',
+
+    
+
+
   });
 }]);
