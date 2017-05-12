336bdd91b872024e871c40a277daa231e1ec5e32
jiazz <jiazz@asiainfo.com>
2017-05-12 19:41:15 +0800

    报告内嵌

--- Basic/app/modules/dataProfile/index.js
+++ Basic/app/modules/dataProfile/index.js
@@ -17,9 +17,11 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
 
     $('#dataPreview').click(function () {
       var filePath = $('#filePath').val();
+      alert(filePath)
       filePath=filePath.replace(/\\/g,"\\\\\\\\");
+      alert(filePath)
 
-      var code = 'f = open("/Users/JiYi/Desktop/dataPreview.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("filePath=","filePath=\\\"'+filePath+'\\\"")\nexec(content)';
+      var code = 'f = open("E:/pycharm/OCAI/CodeIpnb/py/dataPreview.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("filePath=","filePath=\\\"'+filePath+'\\\"")\nexec(content)';
 
       var future = kernel.requestExecute({ code: code });
 
@@ -39,8 +41,8 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
     });
 
     $('#displayReport').click(function () {
-        var htmlFilePath = "/Users/JiYi/Desktop/OCAI/Basic/app/modules/dataProfile/dataProfile.html"
-        var code = 'f = open("/Users/JiYi/Desktop/displayReport.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("htmlFilePath=","htmlFilePath=\\\"'+htmlFilePath+'\\\"")\nexec(content)';
+        var htmlFilePath = "E:/pycharm/OCAI/OCAI/Basic/app/modules/dataProfile/dataProfile.html"
+        var code = 'f = open("E:/pycharm/OCAI/CodeIpnb/py/displayReport.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("htmlFilePath=","htmlFilePath=\\\"'+htmlFilePath+'\\\"")\nexec(content)';
 
         var future = kernel.requestExecute({ code: code });
 
@@ -56,13 +58,23 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
 
         future.onDone = function () {
           //跳转到html页面
-          window.location.href="dataProfile.html";
+          //window.location.href="dataProfile.html";
+          //$("#reportDiv").html("&lt iframe  src='./dataProfile.html' frameborder='0' width='100%' height='500px' &gt &lt/iframe &gt")
+          //$("#reportFrame").reload()
+          $("#reportDiv").css({display:"block"})
+          $("#refreshIframeB").css({display:"block"})
+          $("#refreshIframeB").click()
+          $("#refreshIframeB").css({display:"none"})
+
+
+
+
         };
      });
 
 
     $('#getProcessSuggestions').click(function () {
-          var code = 'execfile("/Users/JiYi/Desktop/getProcessSuggestions.py")';
+          var code = 'execfile("E:/pycharm/OCAI/CodeIpnb/py/getProcessSuggestions.py")';
           var future = kernel.requestExecute({ code: code });
 
           future.onIOPub = function (msg) {
@@ -83,7 +95,7 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
     $('#highCorrProcess').click(function () {
         //deleteCols为要删除的列，多个列以逗号分隔
         var deleteCols = "";
-        var code = 'f = open("/Users/JiYi/Desktop/highCorrProcess.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("deleteCols=","deleteCols=\\\"'+deleteCols+'\\\"")\nexec(content)';
+        var code = 'f = open("E:/pycharm/OCAI/CodeIpnb/py/highCorrProcess.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("deleteCols=","deleteCols=\\\"'+deleteCols+'\\\"")\nexec(content)';
         var future = kernel.requestExecute({ code: code });
 
         future.onIOPub = function (msg) {
@@ -99,7 +111,7 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
     $('#imputerProcess').click(function () {
           //imputerCols需要做空值处理的列，json格式，例如：{'petal width (cm)':'median','petal length (cm)':'mean','sepal width (cm)':'most_frequent','sepal length (cm)':'most_frequent'}
           var imputerCols = "";
-          var code = 'f = open("/Users/JiYi/Desktop/imputerProcess.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("imputerCols=","imputerCols=\\\"'+imputerCols+'\\\"")\nexec(content)';
+          var code = 'f = open("E:/pycharm/OCAI/CodeIpnb/py/imputerProcess.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("imputerCols=","imputerCols=\\\"'+imputerCols+'\\\"")\nexec(content)';
           var future = kernel.requestExecute({ code: code });
 
           future.onIOPub = function (msg) {
@@ -115,7 +127,7 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
     $('#standardProcess').click(function () {
         //standardCols需要做正则化处理的列，json格式，例如：{'petal width (cm)':'Standarded','petal length (cm)':'MinMax','sepal width (cm)':'MaxAbs','sepal length (cm)':'Robust'}
         var standardCols = "";
-        var code = 'f = open("/Users/JiYi/Desktop/standardProcess.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("standardCols=","standardCols=\\\"'+standardCols+'\\\"")\nexec(content)';
+        var code = 'f = open("E:/pycharm/OCAI/CodeIpnb/py/standardProcess.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("standardCols=","standardCols=\\\"'+standardCols+'\\\"")\nexec(content)';
         var future = kernel.requestExecute({ code: code });
 
         future.onIOPub = function (msg) {
@@ -130,7 +142,7 @@ require(['jquery', '@jupyterlab/services'], function ($, services) {
 
     $('#saveData').click(function () {
         var outputFilePath = "";
-        var code = 'f = open("/Users/JiYi/Desktop/saveData.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("saveData=","saveData=\\\"'+saveData+'\\\"")\nexec(content)';
+        var code = 'f = open("E:/pycharm/OCAI/CodeIpnb/py/saveData.py", "r")\ncontent = f.read()\nf.close()\ncontent=content.replace("saveData=","saveData=\\\"'+saveData+'\\\"")\nexec(content)';
         var future = kernel.requestExecute({ code: code });
 
         future.onIOPub = function (msg) {
