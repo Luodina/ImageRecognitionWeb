8ed2b199b401f9050e7864c7582dc793bb3dbac0
2847624779@qq.com <leng0607ning>
2017-05-15 11:03:11 +0800

    页面修改

--- Basic/app/scripts/controllers/dataExplore/data.js
+++ Basic/app/scripts/controllers/dataExplore/data.js
@@ -13,11 +13,34 @@ angular.module('basic')
     $scope.datafileimport = $filter('translate')('web_common_data_explore_006');
     $scope.fileimport = $filter('translate')('web_common_data_explore_007');
     $scope.datapreview = $filter('translate')('web_common_data_explore_008');
+    $scope.next = $filter('translate')('web_common_009');
 
-    $scope.names = {name1:"111"};
-    $scope.tab = "source";
-    $scope.goReport = function(item){
-      console.log("here!!!!", item);
-      $scope.tab =item;
+    // $scope.names = {name1:"111"};
+    // $scope.tab = "source";
+    // $scope.goReport = function(item){
+    //   console.log("here!!!!", item);
+    //   $scope.tab =item;
+    // }
+    $scope.isShow = true;
+    $scope.bigData = '';
+    $scope.testFile = '';
+    $scope.importFlie = function () {
+      // alert('1111');
+      angular.element('#profile_file').click()
+    };
+
+    $scope.fileChange = function (ele) {
+      $scope.testFilePath = ele.value;
+      $scope.$apply();
+    };
+
+    $scope.preview = function () {
+      alert("222");
+
+    };
+
+    $scope.nextPage = function () {
+      $scope.isShow = !$scope.isShow;
     }
-  }]);
+
+}]);
