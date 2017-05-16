695f74a6e687b97a012609675dbab89ae5c701ba
2847624779@qq.com <leng0607ning>
2017-05-16 11:10:41 +0800

    页面修改

--- Basic/app/scripts/controllers/dataExplore/data.js
+++ Basic/app/scripts/controllers/dataExplore/data.js
@@ -14,6 +14,10 @@ angular.module('basic')
     $scope.fileimport = $filter('translate')('web_common_data_explore_007');
     $scope.datapreview = $filter('translate')('web_common_data_explore_008');
     $scope.next = $filter('translate')('web_common_009');
+    $scope.stepone = $filter('translate')('web_common_data_explore_009');
+    $scope.steptwo = $filter('translate')('web_common_data_explore_010');
+    $scope.stepthree = $filter('translate')('web_common_data_explore_011');
+    $scope.application = $filter('translate')('web_common_data_explore_012');
 
     // $scope.names = {name1:"111"};
     // $scope.tab = "source";
@@ -24,6 +28,10 @@ angular.module('basic')
 
     $scope.bigData = '';
     $scope.testFile = '';
+    $scope.isShowOne = false;
+    $scope.isShowTwo = false;
+    $scope.isShowThree = false;
+
     $scope.importFlie = function () {
       // alert('1111');
       angular.element('#profile_file').click()
@@ -41,6 +49,17 @@ angular.module('basic')
 
     $scope.nextPage = function () {
 
+    };
+
+
+    $scope.pulldownlistone = function () {
+      $scope.isShowOne = !$scope.isShowOne;
+    };
+    $scope.pulldownlisttwo = function () {
+      $scope.isShowTwo = !$scope.isShowTwo;
+    };
+    $scope.pulldownlistthree = function () {
+      $scope.isShowThree = !$scope.isShowThree;
     }
 
 }]);
--- Basic/app/scripts/message_en.js
+++ Basic/app/scripts/message_en.js
@@ -21,10 +21,14 @@ angular.module('basic').config(['$translateProvider', function($translateProvide
     'web_common_data_explore_004': 'Data datamation demo',
     'web_common_data_explore_005': 'Default data source Data source (CDM interface) configured with parameter settings',
     'web_common_data_explore_006': 'Data file import',
-    'web_common_data_explore_007': 'import',
+    'web_common_data_explore_007': 'Import',
     'web_common_data_explore_008': 'Data preview',
+    'web_common_data_explore_009': 'Step one  High relevance variable to weight (check the reserved variables)',
+    'web_common_data_explore_010': 'Step two  Variable null processing',
+    'web_common_data_explore_011': 'Step three Variable regularization',
+    'web_common_data_explore_012': 'Application',
 
-    
+
 
 
   });
