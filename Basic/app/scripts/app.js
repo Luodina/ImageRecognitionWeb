161c0d4b66052d8d6fb05eb15c2a477a098586a6
Luodina <marta.rudina@gmail.com>
2017-05-16 11:25:42 +0800

    kernel updates

--- Basic/app/scripts/app.js
+++ Basic/app/scripts/app.js
@@ -38,14 +38,14 @@ angular
       // $urlRouterProvider.when('/explore', '/explore/data');
 
     var states = [
-    { name: 'home',                    url: '/',  templateUrl: "views/dashboard.html"                        },
-    { name: 'dataExplore',             url: '/explore',     templateUrl: 'views/dataExplore/dataExplore.html',  controller: 'DataExploreCtrl'},
-    { name: 'data',        url: '/data',        templateUrl: 'views/dataExplore/data.html',         abstract: true },
-    { name: 'data.report', url: '/report',      templateUrl: 'views/dataReport.html',               controller: 'DataReportCtrl'},
+    { name: 'home',           url: '/',  templateUrl: "views/dashboard.html"                        },
+    { name: 'dataExplore',    url: '/explore',     templateUrl: 'views/dataExplore/dataExplore.html',  controller: 'DataExploreCtrl'},
+    { name: 'data',           url: '/data',        templateUrl: 'views/dataExplore/data.html',         abstract: true },
+    { name: 'data.report',    url: '/report',      templateUrl: 'views/dataReport.html',               controller: 'DataReportCtrl'},
     { name: 'data.source',    url: '/source',      templateUrl: 'views/dataSource.html',               controller: 'DataSourceCtrl' },
     { name: 'data.processing',url: '/processing',  templateUrl: 'views/dataProcessing.html',           controller: 'DataProcessingCtrl' },  
-    { name: 'taskSchedule',url: '/schedule',    templateUrl: 'views/dashboard.html',                controller: 'DashboardCtrl'},
-    { name: 'settings',    url: '/settings',    templateUrl: 'views/settings.html',                 controller: 'SettingsCtrl'}
+    { name: 'taskSchedule',   url: '/schedule',    templateUrl: 'views/dashboard.html',                controller: 'DashboardCtrl'},
+    { name: 'settings',       url: '/settings',    templateUrl: 'views/settings.html',                 controller: 'SettingsCtrl'}
     
     ];
     // Loop over the state definitions and register them
--- Basic/server/api/dataSource.js
+++ Basic/server/api/dataSource.js
@@ -8,12 +8,14 @@ global.WebSocket = WebSocket;
 
 // The base url of the notebook server.
 const BASE_URL = 'http://127.0.0.1:8888/';
-const TOKEN = "c878f8202786ddb4622e05c1577ef81e841e18cce7b6a1d8";
+const TOKEN = "cc41665e3b4f3f7930dd04cdce6819283b278aa7419e63ad";
 let express = require('express');
 let router = express.Router();
-
+let arr =[]
+ console.log('!!!!!!!!!!');
 // Get info about the available kernels and start a new one.
-router.get('/', function(req, res){
+router.get('/jupyter', function(req, res){
+  
   Kernel.getSpecs({ baseUrl: BASE_URL, token:TOKEN, name:'python' }).then(kernelSpecs => {
   console.log('Default spec:', kernelSpecs.default);
   console.log('Available specs', Object.keys(kernelSpecs.kernelspecs));
@@ -28,9 +30,22 @@ router.get('/', function(req, res){
     kernel.requestKernelInfo().then(function (reply) {
       var content = reply.content;
       // $('#kernel-info').text(content.banner);
-      console.log('Kernel info:', content);
-      res.send({msg:"Kernel", kernelInfo : content.banner});;
+      console.log('Kernel info:', content);     
     })
+
+    // Execute and handle replies.
+    let future = kernel.requestExecute({ code: 'print"hello world"' } );
+
+    future.onIOPub = (msg) => {
+      console.log("!!!!!!!!!!!!", );  // Print rich output data.
+      if (msg.header.msg_type === "stream"){return res.send({msg:msg});}
+        
+      // 
+    };
+    future.onDone = () => {
+      console.log('Future is fulfilled', future);    
+    };
+    
   });
   }).catch((err)=>{console.log('error', err);});
 });
