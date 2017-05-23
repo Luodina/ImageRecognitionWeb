f301578ac1a3fb78bd08f8704dc29a2a72932b79
2847624779@qq.com <leng0607ning>
2017-05-23 11:34:40 +0800

    数据探索页面完善

--- Basic/app/scripts/controllers/dataExplore/dataExplore.js
+++ Basic/app/scripts/controllers/dataExplore/dataExplore.js
@@ -7,8 +7,8 @@ angular.module('basic')
     $scope.msg = $filter('translate')('web_common_006');
 
     $scope.listofProject = [
-      {header:"Structural data", content:{key1: "Hey", key2: 'aaa'}, footer:'Add a description'},
-      {header:"Project2", content:{key1: "Hey111", key2: 'aa1111a'}, footer:'Add a description'}
+      {header:"Structural data", content:{Rows: "1", Columns: 'aaa',NumberOfNumericVariables:'',NumberOfCategoryVariables:'',NullRatio:''}, footer:'Add a description'},
+      {header:"Text preprocessing", content:{Ngram: "2", TheNumberOfWords: '1000',NumberOfFeatures:'40998',Weight:'tf-idf'}, footer:'Add a description'}
     ];
     $scope.listofProjectTwo = [
       {header:"Project3", content:"container", footer:'Add a description'},
