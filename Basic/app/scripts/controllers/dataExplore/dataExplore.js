bcdd6234e7e5f33a783bc6563f2983f8486b24e7
2847624779@qq.com <leng0607ning>
2017-05-23 10:34:40 +0800

    数据探索页面``

--- Basic/app/scripts/controllers/dataExplore/dataExplore.js
+++ Basic/app/scripts/controllers/dataExplore/dataExplore.js
@@ -5,9 +5,14 @@
 angular.module('basic')
   .controller('DataExploreCtrl',['$rootScope', '$scope', '$filter', '$http', function ($rootScope, $scope, $filter, $http) {
     $scope.msg = $filter('translate')('web_common_006');
-    
+
     $scope.listofProject = [
-      {header:"Project1", content:"Hey", footer:'End'},
-      {header:"Project2", content:"Heyheyheyhey", footer:'Endend'}
+      {header:"Structural data", content:{key1: "Hey", key2: 'aaa'}, footer:'Add a description'},
+      {header:"Project2", content:{key1: "Hey111", key2: 'aa1111a'}, footer:'Add a description'}
+    ];
+    $scope.listofProjectTwo = [
+      {header:"Project3", content:"container", footer:'Add a description'},
+      {header:"Project4", content:"container container", footer:'Add a description'}
     ]
+
   }]);
