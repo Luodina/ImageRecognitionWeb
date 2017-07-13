d6a93f4186aabeb2ba4401a348c710f0ebc83743
niuniu <niusy@asiainfo.com>
2017-07-14 01:05:13 +0800

    appfile init

--- Basic/app/scripts/controllers/dataApp/appModel/appFile.js
+++ Basic/app/scripts/controllers/dataApp/appModel/appFile.js
@@ -4,9 +4,15 @@
 
 "use strict";
 angular.module('basic')
-  .controller('AppFileCtrl',['createApp','$location','$filter','$scope','projectList',
-  (createApp,$location,$filter,$scope,projectList) => {
+  .controller('AppFileCtrl',['createApp','$location','$filter','$scope','projectList','$http',
+  (createApp,$location,$filter,$scope,projectList,$http) => {
     $scope.appName = $location.path().split(/[\s/]+/).pop();
+    console.log('/api/appFile/init')
+    $http.get('/api/appFile/init', {
+      params: {
+        "appName": $scope.appName
+      }
+    });
     $scope.listAllProject = [];
     let handleSuccess = data => {
       let listAllProject = data.model;
@@ -14,13 +20,13 @@ angular.module('basic')
         listAllProject.forEach(model => {
           if (model.APP_ID !== null && model.APP_ID !== undefined ){
             if (model.TYPE_MENU_ID ==="00" && model.APP_ID === $scope.appName){
-              $scope.listAllProject.push(model);      
+              $scope.listAllProject.push(model);
             }
           }
         });
-      }    
+      }
     };
-    
+
     projectList.get({}, function (res) {handleSuccess(res);});
     $scope.createModel = () => {
       createApp.open();
