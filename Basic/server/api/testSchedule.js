b5cbc7d2ddae1ca2a8abfd0865dc60f76f450616
jiazz <jiazz@asiainfo.com>
2017-07-22 14:58:33 +0800

    data application task schedule

--- Basic/server/api/testSchedule.js
+++ Basic/server/api/testSchedule.js
@@ -7,6 +7,47 @@ let router = express.Router();
 let moment = require('moment');
 let schedule = require('node-schedule');
 
+function getTime(scheduleObj,callback){
+  var time = {hour: scheduleObj.hour, minute: scheduleObj.minute};
+  scheduleObj.DATE?time.date=scheduleObj.DATE:time;
+  scheduleObj.DAYOFWEEK?time.dayOfWeek=scheduleObj.DAYOFWEEK:time;
+  callback(time);
+}
+function newScheduleJob(scheduleName,time,command){
+    schedule.scheduleJob(scheduleName,time,function () {
+      //check the db,if the schedule state == RUNNING
+      MakeSchedule.findOne({
+        where: { SCHEDULE_NAME: scheduleName},
+        raw: true
+      }).then(makeSchedule => {
+        console.log("schedule -------------------",makeSchedule);
+        if(makeSchedule.STATE && makeSchedule.STATE=="RUNNING"){
+          console.log("makeSchedule Running ------------------->",command);
+        }
+      })
+    });
+}
+
+// check the schedule db  if
+function sentinel() {
+  MakeSchedule.findAll({ raw: true })
+    .then(makeSchedule => {
+      console.log("schedule list>>",makeSchedule);
+      makeSchedule.forEach(function (scheduleObj) {
+        //console.log("-------------->>>>",schedule.scheduledJobs[scheduleObj.SCHEDULE_NAME])
+        if(scheduleObj.STATE=="RUNNING"&&!schedule.scheduledJobs[scheduleObj.SCHEDULE_NAME]){
+          getTime(scheduleObj,function (time) {
+            newScheduleJob(scheduleObj.SCHEDULE_NAME,time);
+          })
+        }
+      })
+    })
+    .catch(err =>{console.log('err',err);});
+
+}
+setInterval(sentinel,10000);
+
+
 // SELECT * from APP_MAKESCHEDULE;
 router.get('/getMakeScheduleList', (req, res) => {
     MakeSchedule.findAll({ raw: true })
@@ -72,18 +113,19 @@ router.post('/createSchedule',(req, res) => {
             DAYOFWEEK: time.dayOfWeek,
             isNewRecord:true
         }).then(() => {
-          schedule.scheduleJob(scheduleName,time,function () {
-            //check the db,if the schedule state == RUNNING
-            MakeSchedule.findOne({
-              where: { SCHEDULE_NAME: scheduleName},
-              raw: true
-            }).then(makeSchedule => {
-              console.log("schedule -------------------",makeSchedule);
-              if(makeSchedule.STATE && makeSchedule.STATE=="RUNNING"){
-                console.log("makeSchedule Running -------------------");
-              }
-            })
-          });
+          newScheduleJob(scheduleName,time,command);
+          // schedule.scheduleJob(scheduleName,time,function () {
+          //   //check the db,if the schedule state == RUNNING
+          //   MakeSchedule.findOne({
+          //     where: { SCHEDULE_NAME: scheduleName},
+          //     raw: true
+          //   }).then(makeSchedule => {
+          //     console.log("schedule -------------------",makeSchedule);
+          //     if(makeSchedule.STATE && makeSchedule.STATE=="RUNNING"){
+          //       console.log("makeSchedule Running -------------------");
+          //     }
+          //   })
+          // });
           //console.log("schedule list===========>",schedule.scheduledJobs);
           res.send({ msg:'Success!!!!'});
         }).catch(err =>{
@@ -122,19 +164,20 @@ router.post('/updateScheduleByName',(req, res) => {
         schedule.scheduledJobs[scheduleName].cancel();
         console.log("schedule list afte delte=====edit",schedule.scheduledJobs);
       }
+      newScheduleJob(scheduleName,time,command);
       //create the new chedule with the same name
-      schedule.scheduleJob(scheduleName,time,function () {
-        //check the db,if the schedule state == RUNNING
-        MakeSchedule.findOne({
-          where: { SCHEDULE_NAME: scheduleName},
-          raw: true
-        }).then(makeSchedule => {
-          console.log("schedule -------------------",makeSchedule);
-          if(makeSchedule.STATE && makeSchedule.STATE=="RUNNING"){
-            console.log("makeSchedule Running ------------------->");
-          }
-        })
-      });
+      // schedule.scheduleJob(scheduleName,time,function () {
+      //   //check the db,if the schedule state == RUNNING
+      //   MakeSchedule.findOne({
+      //     where: { SCHEDULE_NAME: scheduleName},
+      //     raw: true
+      //   }).then(makeSchedule => {
+      //     console.log("schedule -------------------",makeSchedule);
+      //     if(makeSchedule.STATE && makeSchedule.STATE=="RUNNING"){
+      //       console.log("makeSchedule Running ------------------->");
+      //     }
+      //   })
+      // });
       //console.log("schedule list===========>",schedule.scheduledJobs);
       res.send({ msg:'Success!!!!'});
     }).catch(err =>{
