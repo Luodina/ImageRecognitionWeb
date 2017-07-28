ad649bee0b0ca1ea582953f92bab44b119ef1a24
2847624779@qq.com <leng0607ning>
2017-07-28 17:23:37 +0800

    运行结果 数据展示

--- Basic/app/scripts/controllers/dataApp/appModel/operationResult.js
+++ Basic/app/scripts/controllers/dataApp/appModel/operationResult.js
@@ -1,13 +1,37 @@
 
 "use strict";
 angular.module('basic')
-  .controller('ResultCtrl',['appOperResult','$rootScope','$filter','$scope',
-  (appOperResult,$rootScope,$filter, $scope) => {
-    $scope.projectType=['web_common_data_app_result_00', 'web_common_data_app_result_01'];
+  .controller('ResultCtrl',['appOperResult','$location','$rootScope','$filter','$scope','$http',
+  (appOperResult,$location,$rootScope,$filter, $scope,$http) => {
+    $scope.appName = $location.path().split(/[\s/]+/).pop();
+    $scope.projectType=[];
+    $scope.projectTypeList=[];
+
+    $http.get('/api/appResults/getScheduleNames/'+ $scope.appName )
+      .success((data)=> {
+        let result = data.results;
+        if (result !== null && result !== undefined){
+          $scope.projectType = result;
+          result.forEach((projectType,  i)=> {
+            console.log('!!!!!projectType',projectType)
+            $http.get('/api/appResults/getResultsList/' + projectType.SCHEDULE_NAME )
+            .success((resList) => {
+              if (resList !== null && resList !== undefined){
+                $scope.projectType[i].resList = resList.resultArr;
+                console.log('111111111111',$scope.projectType[i].resList)
+              }
+            })
+            .catch(err =>{console.log('err', err);})
+          })
+        }
+
+      });
+
+
+
     $scope.view =() => {
       appOperResult.open()
     }
-
   }])
   .directive('result', () => {
     return {
