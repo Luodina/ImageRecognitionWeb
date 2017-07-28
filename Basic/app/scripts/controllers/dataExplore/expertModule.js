594ea79b23460b2e9168ac43bcbb7785a7d130da
Gang Wu <wugang6@asiainfo.com>
2017-07-28 13:46:33 +0800

    添加数据应用中，文件可点击查看功能

--- Basic/app/scripts/controllers/dataExplore/expertModule.js
+++ Basic/app/scripts/controllers/dataExplore/expertModule.js
@@ -4,9 +4,20 @@ angular.module('basic')
   .controller('NotebookCtrl', ['$cookies', '$sce', '$location', '$rootScope', '$scope', '$http',
   function ($cookies, $sce, $location, $rootScope, $scope, $http) {
   $scope.init = function () {
-    var modelName = $location.path().split(/[\s/]+/).pop();
-    console.log('modelName', modelName);
-    $http.get('/api/expert/notebook/open/'+ modelName + '/explore')
+    var path_list = $location.path().split(/[\s/]+/);
+    console.log(path_list);
+    var modelName = path_list.pop();
+    var project = path_list.pop();
+    var type = path_list[1];
+    var url = '/api/expert/notebook/open/'+ modelName +'/';
+    if (type == 'app') {
+      //TODO app中点击文件过来的url是 app/notebook/APP_ID/FILE
+      url += project;
+    } else{
+      url += 'explore'
+    }
+
+    $http.get(url)
     .success( data => {
       console.log('data',data);
       if (data !== null && data !== undefined) {
