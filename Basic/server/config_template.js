f737dda65dcbfb325c65c99644a926023138c405
Luodina <marta.rudina@gmail.com>
2017-11-21 23:13:33 +0800

    config templates

---
+++ Basic/server/config_template.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+    dev: {
+        dist: 'app',
+        port: 9000,
+        notebookUrl: 'http://127.0.0.1:8888/',
+        token: '09b2dc4b7260f502950a27ee106b917cae813ec1f1c22922',
+        notebookPath: '/Users/niuniu/Documents/Project/jupyter-notebooks/',
+        mariadb: 'mariadb://ocai:Ocai@1234@10.1.236.82:3306/ocai',
+
+        // jupyterContainerWorkPath:'',//if not jupyter conatainer in use this value is empty
+        // serverType: 'notebook',   
+        // dataDir: '../data_dir/',
+
+        logTo: 'stdout', // 'stdout' or 'file'. if 'file', logs will be saved in 'logs/server.log'
+        logLevel: 'debug', // 'debug' or 'info'
+    },
+    prod: {
+        dist: 'app',
+        port: 9000,
+        notebookUrl: 'http://@JUPYTER_IP@:@JUPYTER_PORT@/',
+        token: '@JUPYTER_TOKEN@',
+        notebookPath: '@JUPYTER_WORKDIR@',
+        mariadb: 'mariadb://@MYSQL_UNAME@:@MYSQL_PWD@@@MYSQL_ADDR@/@MYSQL_DATABASE@',
+
+        // jupyterContainerWorkPath:'',//if not jupyter conatainer in use this value is empty
+        // serverType: 'notebook',
+        // dataDir: '../data_dir/',
+
+        logTo: 'stdout', // 'stdout' or 'file'
+        logLevel: 'debug' // 'debug' or 'info'
+    },
+    env: 'dev',
+    trans: 'zh'
+};
---
+++ jupyter_config_template.py
@@ -0,0 +1,3 @@
+c.NotebookApp.allow_origin = @JUPYTER_ALLOW_ORIGIN@
+c.NotebookApp.ip = @JUPYTER_IP@
+c.NotebookApp.base_url = @JUPYTER_BASE_URL@
