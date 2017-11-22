bf07b744b6d3886477c5323d873ed7771b4adc9a
Luodina <marta.rudina@gmail.com>
2017-11-22 14:43:06 +0800

    config_templ add jupyterContainerWorkPath

--- Basic/server/config_template.js
+++ Basic/server/config_template.js
@@ -7,11 +7,7 @@ module.exports = {
         token: '09b2dc4b7260f502950a27ee106b917cae813ec1f1c22922',
         notebookPath: '/Users/niuniu/Documents/Project/jupyter-notebooks/',
         mariadb: 'mariadb://ocai:Ocai@1234@10.1.236.82:3306/ocai',
-
-        // jupyterContainerWorkPath:'',//if not jupyter conatainer in use this value is empty
-        // serverType: 'notebook',   
-        // dataDir: '../data_dir/',
-
+        jupyterContainerWorkPath: '', //if not jupyter conatainer in use this value is empty
         logTo: 'stdout', // 'stdout' or 'file'. if 'file', logs will be saved in 'logs/server.log'
         logLevel: 'debug', // 'debug' or 'info'
     },
@@ -22,11 +18,7 @@ module.exports = {
         token: '@JUPYTER_TOKEN@',
         notebookPath: '@JUPYTER_WORKDIR@',
         mariadb: 'mariadb://@MYSQL_UNAME@:@MYSQL_PWD@@@MYSQL_ADDR@/@MYSQL_DATABASE@',
-
-        // jupyterContainerWorkPath:'',//if not jupyter conatainer in use this value is empty
-        // serverType: 'notebook',
-        // dataDir: '../data_dir/',
-
+        jupyterContainerWorkPath: '@JUPYTER_CONTAINER_WORKDIR@', //if not jupyter conatainer in use this value is empty
         logTo: 'stdout', // 'stdout' or 'file'
         logLevel: 'debug' // 'debug' or 'info'
     },
